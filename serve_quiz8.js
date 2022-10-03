fba4a5b5dc52cc42cf92894d557be01a366eec1d
sahanasrini <112678368+sahanahub@users.noreply.github.com>
2022-10-02 23:18:32 -0400

    Initial Commit

---
+++ serve_quiz8.js
@@ -0,0 +1,371 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi  = require('swagger-ui-express');
+const { body, param, validRes } = require('express-validator');
+
+app.use(express.json());
+
+/**
+ * @swagger
+ * components:
+ *     schemas:
+ *         CompanyPost:
+ *             type: object
+ *             properties:
+ *                  companyId:
+ *                       type: string
+ *                       required: true
+ *                  companyName:
+ *                       type: string
+ *                  companyCity:
+ *                       type: string
+ *         CompanyPut:
+ *             type: object
+ *             properties:
+ *                  companyName:
+ *                       type: string
+ *                  companyCity:
+ *                       type: string
+ *         CompanyPatch:
+ *            type: object
+ *            properties:
+ *                 companyName:
+ *                       type: string
+ */
+const options={
+    swaggerDefinition:{
+        openapi: '3.0.0', 
+	    components: {},
+        info: {
+            title: 'Quiz 08',
+            version: '1.0.0',
+            description: 'Using swagger for REST-like API'
+        },
+        host:'157.230.52.160:3002',
+        basePath: '/',
+    },
+    apis: ['./server.js'],
+
+};
+
+const specs = swaggerJsDoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(cors({
+    origin: '*'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+        host : 'localhost',
+        user : 'root',
+        password: 'root',
+        database:  'sample',
+        port: 3306,
+        connectionLimit:5
+});
+
+
+app.get('/', (req, res) => {
+res.send('Hello!');
+});
+
+/**
+ * @swagger
+ * /customer:
+ *   get:
+ *      description: Return all customers
+ *      produces:
+ *           -application/json
+ *      responses:
+ *          200:
+ *              discription: An object of customer containing array of customer code, name, city, working area, company, grade, opening amt, receiving amt, payment amt,
+ *                           outstanding amt, phone number and  agent code.
+ */
+app.get('/customer', async (req, res) => {
+    try {
+        const result = await pool.query("select * from customer");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(result);
+    } catch (err) {
+        throw err;
+    }
+});
+
+/**
+ * @swagger
+ * /company:
+ *   get:
+ *      description: Return all companies
+ *      produces:
+ *           -application/json
+ *      responses:
+ *          200:
+ *              discription: An object of company containing array of company id, name and city.
+ */
+app.get('/company', async (req, res) => {
+    try {
+        const result = await pool.query("select * from company");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(result);
+    } catch (err) {
+        throw err;
+    }
+});
+/**
+ * @swagger
+ * /agents:
+ *   get:
+ *      description: Return all agents
+ *      produces:
+ *           -application/json
+ *      responses:
+ *          200:
+ *              discription: An object of agents containing array of agent id, name, working area, commission, phone number and country.
+ */
+ app.get('/agents', async (req, res) => {
+    try {
+        const result = await pool.query("select * from agents");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(result);
+    } catch (err) {
+        throw err;
+    }
+});
+   
+
+const validateCompany = [body('companyIDCode').exists().withMessage('Company ID is required and cannot be empty').notEmpty().trim().escape(),body('companyName').exists().withMessage('companyName is missing').trim().escape(),body('companyCity').exists().withMessage('companyCity is missing').trim().escape()];
+
+const validateCompanyPatch = [body('companyName').exists().withMessage('companyName is missing').trim().escape()];
+
+const validateCompanyPut = [body('companyName').exists().withMessage('companyName is missing').trim().escape(),body('companyCity').exists().withMessage('companyCity is missing').trim().escape()];
+
+const validateCompanyID = [param('id').exists().withMessage('Company ID is required and cannot be empty').notEmpty().trim().escape()];
+
+/**
+ * @swagger
+ * /company:
+ *     post:
+ *         summary: Insert a new company details
+ *         description: An object of the company is added into the array od company objects
+ *         requestBody:
+ *                required: true
+ *                content:
+ *                   application/json:
+ *                schema:
+ *                       $ref:'#components/schemas/CompanyPost'
+ *         responses:
+ *               200:
+ *                  description: Added into Company
+ *                  content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          items:
+ *                              $ref: '#components/schemas/CompanyPost'
+ *               500:
+ *                  description: Error while inserting the company
+ */
+app.post('/company', validateCompany,async(req,res) => {
+    let response = {result : 'failed', message: 'Error inserting company'};
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+    }
+        try{
+            const reqbody = req.body;
+            const result = await pool.query(
+                "insert into company(COMPANY_ID, COMPANY_NAME, COMPANY_CITY) values ('"+ reqbody.companyID +"', '"+ reqbody.companyName +"','"+ reqbody.companyCity +"')"
+                );
+            if(result.affectedRows){
+                response = {result : 'ok'};
+                res.status(200);
+            } else {
+                res.status(500);
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+    } catch (err) {
+        response.message = err.message;
+        res.status(500);
+        res.json(response);
+    }
+});
+
+/**
+ * @swagger
+ * /company/{id}:
+ *    put:
+ *       description: Api endpoint is used to update data to company table
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: Company ID is required
+ *            schema:
+ *               type: string
+ *       requestBody:
+ *             required: true
+ *             content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#components/schemas/CompanyPut'
+ *       responses:
+ *          200:
+ *              description: Specified Company Updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          items:
+ *                              $ref: '#components/schemas/CompanyPost'
+ *          404:
+ *              description: Company not found in database
+ *          500:
+ *              description: Error updating the Company    
+ */
+
+  app.put('/company/:id', validateCompanyID, validateCompanyPut,async (req, res) => {
+    let response = {result : 'failed', message: 'Error updating data'};
+	const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+    }
+     try {
+         const reqbody = req.body;       
+         const result = await pool.query("UPDATE company SET COMPANY_NAME = '"+reqbody.companyName+"', COMPANY_CITY = '"+reqbody.companyCity+"' WHERE COMPANY_ID = '"+ req.params.id +"'");
+         
+	    if (result.affectedRows) {
+             response = {result : 'ok'};
+             res.status(200);
+         }  else {
+            res.status(404);
+            response.message = "Company not found in the database";
+         }
+         res.setHeader('Content-Type', 'application/json');
+         res.json(response);
+     } catch (err) {
+         response.message = err.message;
+         res.status(500);
+         res.json(response);
+     }
+ });
+
+/**
+ * @swagger
+ * /company/{id}:
+ *    patch:
+ *       description: API is used to update only a specific fields to specified company row
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: Company ID is required
+ *            schema:
+ *               type: string
+ *       requestBody:
+ *             required: true
+ *             content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#components/schemas/CompanyPatch'
+ *       responses:
+ *          200:
+ *            description: Company updated with Name
+ *            content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          items:
+ *                              $ref: '#components/schemas/CompanyPatch'
+ *          404:
+ *              description: Company not found in database
+ *          500:
+ *              description: error updating the Company data 
+ */
+
+  app.patch('/company/:id', validateCompanyID, validateCompanyPatch ,async (req, res) => {
+    let response = {result : 'failed', message: 'Error updating Company Name'};
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+    }
+     try {
+         const reqbody = req.body; 
+         const result = await pool.query("UPDATE company SET  COMPANY_NAME = '"+reqbody.companyName+"' WHERE COMPANY_ID = '"+ req.params.code +"'");
+
+         if (result.affectedRows) {
+             response = {result : 'ok'};
+             res.status(200);
+         } else {
+            res.status(404);
+            response.message = "Company not found in the database";
+         }
+         res.setHeader('Content-Type', 'application/json');
+         res.json(response);
+     } catch (err) {
+         response.message = err.message;
+         res.status(500);
+         res.json(response);
+     }
+ });
+
+/**
+ * @swagger
+ * /company/{id}:
+ *    delete:
+ *       description: API is used to delete record from company table
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: CompanyID is required
+ *            schema:
+ *               type: string
+ *       responses:
+ *          200:
+ *              description: Specified Company deleted
+ *          404:
+ *              description: Specified Company not found in database
+ *          500:
+ *              description: error deleting the Specified Company 
+ */
+  app.delete('/company/:id', validateCompanyID,async (req, res) => {
+    let response = {result : 'failed', message: 'Error deleting Company'};
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+    }
+     try {    
+         const result = await pool.query("DELETE FROM company WHERE COMPANY_ID = '"+req.params.code+"'");
+ 
+         if (result.affectedRows) {
+             response = {result : 'ok'};
+             res.status(200);
+         } else {
+            res.status(404);
+            response.message = "Specified Company not found in the database";
+         }
+         res.setHeader('Content-Type', 'application/json');
+         res.json(response);
+     } catch (err) {
+         response.message = err.message;
+         res.status(500);
+         res.json(response);
+     }
+ });
+   
+    
+app.listen(port, () => {
+    console.log('Server_Quiz8 app listening at http://localhost:${port}',port);
+    
+});
+    
